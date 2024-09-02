7c15c9bb5c8b689f4102efe1a931def4d4b24ba4
Agalya-10 <aagalya360@gmail.com>
2024-09-02 16:52:35 +0530

    using function

--- main.js
+++ main.js
@@ -1,12 +1,91 @@
+function helloDate() {
+    document.getElementById('dateDisplay').innerHTML = new Date();
+}
+// variables
+var a=90;
+var b=20;
+console.log(a-b)
 
+let price=1000;
+var product = "laptop";
+let tax=2500;
+const total=price+tax;
+console.log(product)
+console.log(total)
+ 
+ var fruitName="apple";
+ var count=10;
+ var apple=5;
+ const totalprice=(count*apple);
+ console.log(fruitName);
+  console.log(totalprice);
+//   operator
+  var a=20;
+  a++;
+  console.log(a);
+   
+  var a=20;
+  b=a++;
+  console.log(b);
+  
+  var a=60;
+  b=--a;
+  console.log(b);
+  
+  var a=true;
+console.log(typeof(a));
+var name="agalya";
+console.log("my name is",name)
+console.log(typeof(name));
+var c;
 
+console.log(typeof(c));  
 
+// function
+function myNameIs()
+{
+   console.log( "I am Agalya from thanjavur")
+}
+myNameIs()
 
+function fruit()
+{
+   console.log( "this is apple")}
+   
+function pen(){
+   console.log( "this is pen") }
+   
+function lap(){
+   console.log( "this is mouse")
+}
+fruit()
+pen()
+lap()
 
+var a=40;
+var b=50;
+function sub()
+{
+    console.log(a*b)
+}
+sub()
 
+var actor="surya";
+var player="dhoni";
+var movie="friends";
+function favorite(){
+    console.log(" My fav Hero : " + actor)
+    console.log(" My fav Player : ",player )
+    console.log(" My fav Movie : " ,movie )
+}
+favorite()
 
+// parameter
 
-
+function area(l,b){
+    console.log(l+b);
+}
+area(55,75)
 
 
 
