2bac58d79d92e6fdb4a5ca68961758c2756b4cbf
Agalya-10 <aagalya360@gmail.com>
2024-09-04 16:58:11 +0530

    using ifelse

--- main.js
+++ main.js
@@ -132,7 +132,38 @@ else{
     }
 
 
-
+    var color="yellow"
+    if(color=="yellow")
+    {
+        console.log("get ready")
+    }
+    if (color=="red")
+    {
+       console.log("stop") 
+    }
+    if (color=="green")
+    {
+       console.log("go") 
+    }
+    
+    var season="spring"
+    
+    if(season=="winter")
+    {
+        console.log("bundle up and stay warm")
+    }
+    if(season=="spring")
+    {
+        console.log("enjoy the blooming flowers")
+    }
+    if(season=="summer")
+    {
+        console.log("have fun in the sun")
+    }
+    if(season=="autumn")
+    {
+        console.log("admire the colorfull leaves")
+    }
 
 
 
